9a49aa0dba5c54c4d60ceda9b50cbab0d9db1df6
Josep M Sobrepere <jm.sobrepere@gmail.com>
2023-07-13 14:37:20 +0200

    finish vanilla solution

--- src/solutions/vanilla.ts
+++ src/solutions/vanilla.ts
@@ -49,6 +49,39 @@ export default suite("vanilla", {
         })
     })
   },
+  ignoreNewOnesUntilResolved({
+    listenToHashes,
+    getBlockNumber,
+    getBlockTime,
+    output,
+  }) {
+    let isListening = true
+    let isRunning = false
+
+    const stopListening = listenToHashes((hash) => {
+      if (hash === "h") {
+        stopListening()
+        isListening = false
+      }
+
+      if (isRunning) return
+
+      isRunning = true
+      getBlockTime(hash)
+        .then((blockTime) => ({
+          hash,
+          blockTime,
+          blockNumber: getBlockNumber(hash),
+        }))
+        .then((x) => {
+          isRunning = false
+          output(x)
+          if (!isListening) {
+            output(null)
+          }
+        })
+    })
+  },
   newOneCancelsOldOne: ({
     listenToHashes,
     getBlockNumber,
@@ -81,6 +114,40 @@ export default suite("vanilla", {
         })
         .catch((e: Error) => {
           if (e.message !== "aborted") throw e
+        })
+    })
+  },
+  allInOrder({ listenToHashes, getBlockNumber, getBlockTime, output }) {
+    let isListening = true
+    const onHold = new Map<number, any>()
+    let nextidx = 0
+    let waitingForIdx = 0
+
+    const stopListening = listenToHashes((hash) => {
+      let idx = nextidx++
+
+      if (hash === "h") {
+        stopListening()
+        isListening = false
+      }
+
+      getBlockTime(hash)
+        .then((blockTime) => ({
+          hash,
+          blockTime,
+          blockNumber: getBlockNumber(hash),
+        }))
+        .then((x) => {
+          if (idx !== waitingForIdx) return onHold.set(idx, x)
+
+          output(x)
+
+          while (onHold.has(++waitingForIdx)) {
+            output(onHold.get(waitingForIdx))
+            onHold.delete(waitingForIdx)
+          }
+
+          if (!isListening) output(null)
         })
     })
   },
